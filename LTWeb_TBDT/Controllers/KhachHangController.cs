33c321aa74aed8fe230580ac7091ea5b7597c6f6
chuong1210 <chuongbd1012@gmail.com>
2024-10-23 21:31:39 +0700

    update

---
+++ LTWeb_TBDT/Controllers/KhachHangController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LTWeb_TBDT.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
