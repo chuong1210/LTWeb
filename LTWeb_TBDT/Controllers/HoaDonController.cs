2165ce618cb66c8a2c0701c73862d50d545c120a
minhluan2421 <nguyenminhluan0824@gmail.com>
2024-12-12 08:52:47 +0700

    cm all

---
+++ LTWeb_TBDT/Controllers/HoaDonController.cs
@@ -0,0 +1,60 @@
+﻿using LTWeb_TBDT.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LTWeb_TBDT.Controllers
+{
+    public class HoaDonController : Controller
+    {
+        private readonly BanThietBiDienTuContext db;
+        private readonly ConnectHoaDon _connectHoaDon;
+        private readonly ConnnectChiTietHoaDon _connectChiTietHoaDon;
+
+        public HoaDonController(BanThietBiDienTuContext context, ConnectHoaDon connectHoaDon, ConnnectChiTietHoaDon connectChiTietHoaDon)
+        {
+            db = context;
+            _connectHoaDon = connectHoaDon;
+            _connectChiTietHoaDon = connectChiTietHoaDon;
+
+        }
+        public IActionResult Index(int? id)
+        {
+            if (id.HasValue)
+            {
+                var hoadon = db.HoaDons.Where(hd => hd.MaHoaDon == id).FirstOrDefault();
+                hoadon.TrangThai = "Đã duyệt";
+                db.Update(hoadon);
+                db.SaveChanges();
+
+            }
+            return View(db.HoaDons.ToList());
+        }
+        public IActionResult DuyetDon(int id)
+        {
+            var hoadon = db.HoaDons.Where(hd => hd.MaHoaDon == id).FirstOrDefault();
+            hoadon.TrangThai = "Đã duyệt";
+            db.Update(hoadon);
+            db.SaveChangesAsync();
+            return RedirectToAction("Index", db.HoaDons.ToList());
+        }
+        public IActionResult Detail(int id)
+        {
+
+            {
+                var order = db.HoaDons
+                                          .Include(hd => hd.ChiTietHoaDons)
+                                          .ThenInclude(cthd => cthd.MaSanPhamNavigation)
+                                          .FirstOrDefault(hd => hd.MaHoaDon == id);
+
+                if (order == null)
+                {
+                    return NotFound();
+                }
+
+                return View(order);
+            }
+
+
+        }
+    }
+}
--- LTWeb_TBDT/obj/Debug/net6.0/LTWeb_TBDT.AssemblyInfo.cs
+++ LTWeb_TBDT/obj/Debug/net6.0/LTWeb_TBDT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LTWeb_TBDT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e72a23f199db88633b485947c401dfb83ee21ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31240e1d0e0458814cbd684fa2118c8ad00c21d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LTWeb_TBDT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LTWeb_TBDT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
