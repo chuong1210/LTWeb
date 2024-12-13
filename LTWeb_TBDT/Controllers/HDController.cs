0090666966db1d622b4330952eaba579b68de686
chuong1210 <chuongbd1012@gmail.com>
2024-12-13 16:14:10 +0700

    sa

---
+++ LTWeb_TBDT/Controllers/HDController.cs
@@ -0,0 +1,28 @@
+﻿using LTWeb_TBDT.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LTWeb_TBDT.Controllers
+{
+    public class HDController : Controller
+    {
+        private readonly BanThietBiDienTuContext db;
+        private readonly ConnectHoaDon _connectHoaDon;
+        private readonly ConnnectChiTietHoaDon _connectChiTietHoaDon;
+        public HDController(BanThietBiDienTuContext context, ConnectHoaDon connectHoaDon, ConnnectChiTietHoaDon connectChiTietHoaDon)
+        {
+            db = context;
+
+            _connectHoaDon = connectHoaDon;
+            _connectChiTietHoaDon = connectChiTietHoaDon;
+        }
+        public IActionResult Index()
+        {
+            return View(db.HoaDons.ToList());
+        }
+
+
+
+     
+        }
+    }
+
